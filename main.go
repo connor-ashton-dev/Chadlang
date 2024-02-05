ce23525b5991444e0460111f9a9cddf9589baeb4
Connor Ashton <cjconnorashton@gmail.com>
2024-02-04 21:31:47 -0700

    good progress with lexer, REPL good too

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/connor-ashton-dev/monkey/repl"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Println("Feel free to type in commands")
+	repl.Start(os.Stdin, os.Stdout)
 }
